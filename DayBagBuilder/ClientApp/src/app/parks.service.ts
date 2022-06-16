0e05032e217c0a30e3d4ec8ea3d56909254be93e
FoxFunctions <95949981+FoxFunctions@users.noreply.github.com>
2022-06-16 15:01:29 -0400

    Update parks.service.ts

--- DayBagBuilder/ClientApp/src/app/parks.service.ts
+++ DayBagBuilder/ClientApp/src/app/parks.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Parks } from './Parks';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParksService {
+locationString: string = "";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  ShowParkActivites(): Observable <Parks>{
+    return this.http.get<Parks>("https://developer.nps.gov/api/v1/activities?q="+this.locationString+"&api_key=kXqcWPcYb2fgVVAVO30bfMdi217VOjEgkWdL7uLV");
+  }
+
 }
